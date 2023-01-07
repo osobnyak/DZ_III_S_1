fe96f6daf9a00e6e25359210a8256dccfd98ff6d
Vladimir.S <vibiray2@mail.ru>
2023-01-07 20:22:34 +0400

    задание 3 выполнено

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//получаем число. Определить чётное или нет.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numOst = num%2;
+
+if (numOst == 0)
+{
+    Console.WriteLine("да, чётное");
+}
+else 
+{
+    Console.WriteLine("нет, не чётное");
+}

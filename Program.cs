ab96c34c5123c53718b8ec4aac66b4a7980937c2
Vladimir.S <vibiray2@mail.ru>
2023-01-07 18:34:02 +0400

    задание 1 выполнено

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
 ﻿//branch = 1
+//на входе 2 числа. Определить какое больше, какое меньше. 
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+{
+    Console.Write(num1);
+    Console.Write(">");
+    Console.WriteLine(num2);
+    Console.Write("max=");
+    Console.WriteLine(num1);
+}
+if (num1<num2)
+{
+    Console.Write(num1);
+    Console.Write("<");
+    Console.WriteLine(num2);
+    Console.Write("max=");
+    Console.WriteLine(num2);
+}
+if (num1==num2)
+{
+    Console.WriteLine("числа равны");
+}
